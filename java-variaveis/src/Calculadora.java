33755fbf4a18a7899f138dbba19d9f07f0621584
Programação-java <kaue171717@gmail.com>
2025-04-28 22:05:50 -0300

    Adicionando arquivos do projeto

---
+++ java-variaveis/src/Calculadora.java
@@ -0,0 +1,20 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        // Definição do valor base
+        double valor = 2600;
+
+        // Cálculo de 40% do valor
+        double percentual = valor * 0.40;
+        double percentual2 = valor * 0.60; // Cálculo de 60% do valor
+
+        // Cálculo do módulo
+        int modulo1 = 2600 % 40; // Resto da divisão de 2600 por 40
+        int modulo2 = 2600 % 60; // Resto da divisão de 2600 por 60
+
+        // Exibe os resultados no console
+        System.out.println("40% de " + valor + " é: " + percentual);
+        System.out.println("60% de " + valor + " é: " + percentual2);
+        System.out.println("O valor do módulo 1 (2600 % 40) é: " + modulo1);
+        System.out.println("O valor do módulo 2 (2600 % 60) é: " + modulo2);
+    }
+}
