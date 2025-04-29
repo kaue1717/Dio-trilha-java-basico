ce4e93392b2efebc01e8fab529fcc75dc3d0744f
Programação-java <kaue171717@gmail.com>
2025-04-28 21:34:27 -0300

    Adicionando mudanças no readme e na pasta app

---
+++ java-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,14 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+        salarioMinimo = 1000;
+
+
+        // tipos Primitivos 
+
+        System.out.println(salarioMinimo);
+
+        // final double PI = 3.14; // Constante
+        
+    }
+}
