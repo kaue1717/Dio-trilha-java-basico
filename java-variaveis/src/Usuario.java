8c587566c371a43ac840fbcd43cb217445051b4a
Programação-java <kaue171717@gmail.com>
2025-04-29 22:41:39 -0300

    Adicione todas ás ultimas alterações feitas por mim . Inclusive das novas pastas e arquivos

---
+++ java-variaveis/src/Usuario.java
@@ -0,0 +1,16 @@
+public class Usuario {
+    public static void main(String[]) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual :? " + smartTv.canal);
+       // System.out.println("Volume Atual :? " + smartTv.volume);
+
+
+       // smartTv.ligar (); 
+
+        //System.out.printl("TV Ligada ?" + smartTv.ligada);
+ 
+    }
+    
+}
